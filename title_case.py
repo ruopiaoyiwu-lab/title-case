279bdc34b6e7dc37e8533006454c59482b0773d0
ruopiaoyiwu <ruopiaoyiwu@uchicago.edu>
2026-09-18 13:47:14 -0500

    Add title case function

--- title_case.py
+++ title_case.py
@@ -5,3 +5,10 @@
 
 def my_cap(text):
     return text[0].upper() + text[1:]
+
+
+def my_title(text):
+    words = []
+    for t in text.split():
+        words.append(my_cap(t))
+    return " ".join(words)
